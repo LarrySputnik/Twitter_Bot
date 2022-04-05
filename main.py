dd00e919b8ec3c978ddb0297b55ae8e6477994fc
LarryAnderson <larryandersonwork@gmail.com>
2022-04-05 00:18:28 -0700

    functions working

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import tweepy
 import os
 from random import choice, randint
@@ -18,20 +19,16 @@ def get_client():
     return client
 
 
+def new_tweet(text):
+    """Create a new tweet for Larry Anderson"""
+    sputnik.create_tweet(text=text)
+    print("success")
+
+
 def get_user_details(username):
     """Get twitter ID, Name and Username for chosen account, username=string twitter handle without @ """
     user_info = sputnik.get_users(usernames=username)
     print(user_info)
-
-
-def new_tweet(text):
-    """Create a new tweet for Larry Anderson"""
-    sputnik.create_tweet(text=text)
-
-
-def reply_to_tweet(tweet_id_number, text):
-    """reply to a tweet"""
-    sputnik.create_tweet(in_reply_to_tweet_id=tweet_id_number, text=text)
 
 
 def get_last_tweet_id(user_id):
@@ -39,50 +36,46 @@ def get_last_tweet_id(user_id):
     last_tweet = sputnik.get_users_tweets(user_id, exclude=["retweets", "replies"])
     tweet_meta = last_tweet.meta
     result = int(tweet_meta['newest_id'])
+    print(result)
     return result
 
 
-users = {
-    "donlemon": 16051471,
-    "RepAdamSchiff": 29501253,
-    "JoeBiden": 939091,
-    "MSNBC": 2836421,
-    "cnn": 759251
-}
+def get_mention():
+    """Return the tweet id number for the last person who mentioned Larry"""
+    mentions = sputnik.get_users_mentions(1503766612519706627)
+    mention_data = mentions.data
+
+    results = []
+    if mention_data:
+        for tweet in mention_data:
+            results.append(int(tweet.id))
+        print(results[0])
+        return results[0]
+
+
+def respond_to_tweet(tweet_id_number):
+    """Respond to a tweet that mentions Larry"""
+    # Get the user's information who sent the tweet
+    username = sputnik.get_tweet(id=tweet_id_number, expansions=["author_id"])
+    # gets just the username of the person who sent a message to Larry
+    ids = username.includes["users"][0]
+
+    with open("insult_info.json", "r") as f:
+        data = json.load(f)
+        text = choice(data)
+
+        with open("template1.txt") as letter:
+            contents = letter.read()
+            contents = contents.replace("[NAME]", str(ids))
+            contents = contents.replace("[TEXT]", text)
+
+    sputnik.create_tweet(in_reply_to_tweet_id=tweet_id_number, text=contents)
+
 
 # Activate Sputnik
 sputnik = get_client()
 
-# Hold all the last tweet id numbers
-tweet_id_list = []
 
-# Hold the last tweets that Larry has sent out
-tweet_list = []
+respond_to_tweet(1511195457581592580)
 
-while True:
-    for person, person_id in users.items():
-        last_tweet_id = get_last_tweet_id(person_id)
-        if last_tweet_id not in tweet_id_list:
-            tweet_id_list.append(last_tweet_id)
-            with open("trump_quotes.txt", "r") as file:
-                quotes = file.readlines()
-                random_reply = choice(quotes)
-                final_tweet = "".join(random_reply)
-                reply_to_tweet(tweet_id_list[-1], final_tweet)
-        else:
-            pass
-    tweet_timer = randint(3000, 25000)
-    time.sleep(tweet_timer)
-    with open("trump_quotes.txt", "r") as file:
-        number_of_quotes = file.readlines()
-        random_quote = choice(number_of_quotes)
 
-    if random_quote not in tweet_list and len(random_quote) <= 280:
-        tweet_list.append(random_quote)
-        final_tweet = "".join(tweet_list[-1])
-        new_tweet(final_tweet)
-    else:
-        pass
-
-    if len(tweet_list) == len(number_of_quotes):
-        tweet_list.clear()
